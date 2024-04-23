f315f5fc062e66d0f1f1e23af2a180fd743aaed6
fabulaye <53220111+fabulaye@users.noreply.github.com>
2024-04-23 12:55:05 +0200

    new wheel

--- lukasdata/setup.py
+++ lukasdata/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='lukasdata',
     packages=find_packages(),
-    version="1.0",
+    version="1.1",
     install_requires=["numpy","pandas","matplotlib"
     ]
 )
