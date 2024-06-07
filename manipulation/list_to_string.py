7684bb6616a6e510fa258d1d6180510e6de0362c
fabulaye <53220111+fabulaye@users.noreply.github.com>
2024-06-07 21:43:25 +0200

    list functions

--- manipulation/list_to_string.py
+++
@@ -1,5 +0,0 @@
-def list_to_string(list):
-    full_string=""
-    for string in list:
-        full_string=full_string+" "+string
-    return full_string    
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='lukasdata',
     packages=find_packages(),
-    version='1.4.1',
+    version='1.4.2',
     install_requires=["numpy","pandas","matplotlib"
     ]
 )
