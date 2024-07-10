43d27218c12cf4a5b83b4b4a32bdf106b9c3e097
fabulaye <53220111+fabulaye@users.noreply.github.com>
2024-07-10 23:51:16 +0200

    new functionss

--- exploration/nan_heatmap.py
+++ exploration/nan_heatmap.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from datahandling.change_directory import chdir_sql_requests
+from cleaning.drop_column_with_na import drop_nan_columns
 def nan_heatmap(df,title="NaN Values"):
 
     plt.figure(figsize=(20, 10))
@@ -13,3 +16,10 @@ def nan_heatmap(df,title="NaN Values"):
     plt.ylabel('Rows')
     plt.show()
 
+
+chdir_sql_requests()
+df=pd.read_csv("financialsbvd_ama.csv")
+df=drop_nan_columns(df,0.2)
+print(df.columns)
+
+nan_heatmap(df)
