0233672d91b9369ef335f09e6bea7bf2af3f409f
fabulaye <53220111+fabulaye@users.noreply.github.com>
2024-07-10 23:52:38 +0200

    new update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='lukasdata',
     packages=find_packages(),
-    version='1.4.4',
+    version='1.4.5',
     install_requires=["numpy","pandas","matplotlib"
     ]
 )
